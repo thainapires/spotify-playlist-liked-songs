c7d06c8cb4d3391dd3f26d849daf7682286a1431
Thainá Pires <thainaspiress@gmail.com>
2020-10-12 01:25:17 -0300

    Creating playlist and populating

--- Create_Playlist.py
+++ Create_Playlist.py
@@ -2,22 +2,52 @@ from secrets import spotify_user_id, spotify_token
 import requests
 import json
 
-#Helps to print json response
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-
+uris = [] 
 query = f'https://api.spotify.com/v1/me/tracks?offset=0&limit=10' #Returns last 10 songs in the saved playlist
 
 #Requesting and getting response
 response = requests.get(query, 
                headers={"Content-Type":"application/json", 
-                        "Authorization":f"Bearer {token}"})
+                        "Authorization":f"Bearer {spotify_token}"})
 json_response = response.json()
 
+#print(response.status_code)
+
 #Printing response
-print("Last 10 songs in the saved playlist:")
+#print("Last 10 songs in the saved playlist:")
 for i,j in enumerate(json_response['items']):
-    print(f"{i+1}) \"{j['track']['name']}\"")
+    uris.append(j['track']['uri'])
+#    print(f"{i+1}) \"{j['track']['name']}\"")
+
+#Creating the playlist 
+
+query2 = f"https://api.spotify.com/v1/users/{spotify_user_id}/playlists"
+
+request_body = json.dumps({
+          "name": "Last 10 songs saved",
+          "description": "The last 10 songs that I've saved",
+          "public": False
+})
+
+response2 = requests.post(url = query2, data = request_body, headers={"Content-Type":"application/json", 
+                        "Authorization":f"Bearer {spotify_token}"})
+
+url = response2.json()['external_urls']['spotify']
+
+#print(response2.status_code)
+
+#Filling the playlist with the songs
+
+playlist_id = response2.json()['id']
+
+query3 = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+
+request_body = json.dumps({
+          "uris" : uris
+        })
+response3 = requests.post(url = query3, data = request_body, headers={"Content-Type":"application/json", 
+                        "Authorization":f"Bearer {spotify_token}"})
+
+#print(response3.status_code)
+
+print(f'Your playlist is ready at {url}')
